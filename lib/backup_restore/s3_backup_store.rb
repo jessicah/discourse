5aa6ccdcacdf9d187ebfb10f9d30d38417c35241
Andrew Schleifer <me@andrewschleifer.name>
2020-05-07 12:58:53 +0800

    DEV: harmonize s3 uploads location
    i.e.
    - remove MULTISITE_PREFIX, and
    - always include dbname
    
    currently, with $S3_BACKUP_BUCKET of "bucket/backups", multisite backups
    end up in "bucket/backups/backups/dbname/" and single-site are in
    "bucket/backups/" -- but both _should_ be in "bucket/backups/dbname/"
    
    TODO: migrate existing backups

--- lib/backup_restore/s3_backup_store.rb
+++ lib/backup_restore/s3_backup_store.rb
@@ -3,7 +3,6 @@
 module BackupRestore
   class S3BackupStore < BackupStore
     UPLOAD_URL_EXPIRES_AFTER_SECONDS ||= 21_600 # 6 hours
-    MULTISITE_PREFIX = "backups"
 
     def initialize(opts = {})
       s3_options = S3Helper.s3_options(SiteSetting)
@@ -99,11 +98,7 @@ module BackupRestore
     end
 
     def s3_bucket_name_with_prefix
-      if Rails.configuration.multisite
-        File.join(SiteSetting.s3_backup_bucket, MULTISITE_PREFIX, RailsMultisite::ConnectionManagement.current_db)
-      else
-        SiteSetting.s3_backup_bucket
-      end
+      File.join(SiteSetting.s3_backup_bucket, RailsMultisite::ConnectionManagement.current_db)
     end
 
     def file_regex
